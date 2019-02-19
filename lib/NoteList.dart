2a597968be0a38c4977516762b9cd6e972f7ace3
Fahri Fauzan <fahriifauzan32@gmail.com>
2019-02-19 15:20:44 +0700

    Add Note List Screen and Note Detail Screen

---
+++ lib/NoteList.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class NoteListPage extends StatefulWidget {
+  @override
+  _NoteListPageState createState() => _NoteListPageState();
+}
+
+class _NoteListPageState extends State<NoteListPage> {
+  int count = 0;
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'To Do List Note',
+          style: TextStyle(
+            color: new Color(0xffFFFFFF),
+          ),
+        ),
+      ),
+
+      body: getNoteListView(),
+
+      floatingActionButton: FloatingActionButton(
+		    onPressed: () {
+		      debugPrint('FAB clicked');
+		    },
+
+		    tooltip: 'Add Note',
+
+		    child: new Icon(
+          Icons.add,
+          color: new Color(0xffFFFFFF)
+        ),
+	    ),
+    );
+  }
+  ListView getNoteListView(){
+    TextStyle titleStyle = Theme.of(context).textTheme.subhead;
+
+
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.yellow,
+							child: Icon(Icons.keyboard_arrow_right),
+            ),
+            title: Text('Dummy Title', style: titleStyle,),
+						subtitle: Text('Dummy Date'),
+						trailing: Icon(Icons.delete, color: Colors.grey,),
+						onTap: () {
+							debugPrint("ListTile Tapped");
+						},
+          ),
+        );
+      }
+    );  
+  }
+}
---
+++ lib/SplashScreen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = new Duration(seconds: 5);
+    return new Timer(_duration, navigationPage);
+  }
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/NoteDetail');
+  }
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Stack(
+        fit:StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: new Color(0xff0087BA),
+              gradient: LinearGradient(
+                colors: [new Color(0xff0087BA), new Color(0xff61C199)],
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+              )
+            )
+          ),
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text (
+                'CRUD',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 55.0,
+                  letterSpacing: 20.0,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 0.0),
+              ),
+              Text (
+                'To Do List App',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                  // letterSpacing: 10.0
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
